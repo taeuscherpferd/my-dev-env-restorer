5fc55827623e84e20c124529e164f2af55e6b6eb
taeuscherpferd <ibber@myokcorral.com>
2022-07-26 10:28:40 -0600

    Many updates to this file

--- Windows/powerShell/Microsoft.PowerShell_profile.ps1
+++ Windows/powerShell/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,40 @@
 Set-Alias -Name o -Value open-cur-dir
 Set-Alias -Name WN -Value nav-to-nimbus
+Set-Alias -Name WD -Value  nav-to-work-projects 
 
 function open-cur-dir {
     ii .
 }
 
 function nav-to-nimbus {
-    Set-Location -Path "D:\wprojects\nimbus\Platform\ui"
+    Set-Location -Path "D:\wprojects\nimbus\ui"
+}
+
+function nav-to-work-projects {
+    Set-Location -Path "D:\wprojects"
+}
+
+function npm-clean {
+  rm -r .\node_modules\, .\package-lock.json;
+  npm i;
+  npm audit fix;
+  npm start;
+}
+
+function mm {
+  git pull --rebase;
+}
+
+function npmi($a) {
+  npm i $a; npm i -D @types/$a;
+}
+
+function npmd($a) {
+  npm i -D $a;
+}
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
 }
